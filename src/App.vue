5507e1d323bb681ef16c600f15b11873b947694f
Andy <68723584+LovelyAndy@users.noreply.github.com>
2021-07-25 10:57:14 -0500

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="app">
-    <h1>Milwaukee Breweries List</h1>
+    <img src="/mke.svg" alt="milwaukee in text" class="_mke" />
+    <h1 class="_main-title">Brewery List</h1>
     <form class="_form" @submit.prevent="submit">
-      City
-      <select name="breweries" id="_breweries" v-model="selectedCity">
-        <option value="">All</option>
-        <option value="milwaukee">Milwaukee</option>
-      </select>
-      <!-- <div>{{ selectedCity }}</div> -->
-      Type
-      <select name="types" id="_types" v-model="selectedType">
-        <option value="">All</option>
-        <option value="micro">Micro</option>
-        <option value="nano">Nano</option>
-        <option value="regional">Regional</option>
-        <option value="brewpub">Brewpub</option>
-        <option value="large">Large</option>
-        <option value="planning">Planning</option>
-        <option value="bar">Bar</option>
-        <option value="contract">Contract</option>
-        <option value="proprietor">Proprietor</option>
-        <!-- <option value="closed">Closed</option> -->
-      </select>
-      <button type="submit" class="_btn" alt="submit form">Submit</button>
-      <ul class="_selected">
+      <div class="_filter-bar">
+        <div class="_filter">
+          <div class="_filter-title">City</div>
+          <select class="_dropdown" name="breweries" id="_breweries" v-model="selectedCity">
+            <option value="">All</option>
+            <option value="milwaukee">Milwaukee</option>
+          </select>
+        </div>
+        <!-- <div>{{ selectedCity }}</div> -->
+        <div class="_filter">
+          <div class="_filter-title">Type</div>
+          <select class="_dropdown" name="types" id="_types" v-model="selectedType">
+            <option value="">All</option>
+            <option value="micro">Micro</option>
+            <option value="nano">Nano</option>
+            <option value="regional">Regional</option>
+            <option value="brewpub">Brewpub</option>
+            <option value="large">Large</option>
+            <option value="planning">Planning</option>
+            <option value="bar">Bar</option>
+            <option value="contract">Contract</option>
+            <option value="proprietor">Proprietor</option>
+            <!-- <option value="closed">Closed</option> -->
+          </select>
+        </div>
+        <button type="submit" class="_btn" alt="submit form">Submit</button>
+      </div>
+      <ul class="_list">
         <li
           class="_brewery"
           v-for="brewery in visibleBreweries"
@@ -31,29 +38,24 @@
           :visibleBreweries="visibleBreweries"
           :currentPage="currentPage"
         >
-          <div v-if="brewery.state">
-            <div class="_title">State:</div>
-            <div class="_info">{{ brewery.state }}</div>
+          <div v-if="brewery.name" class="_col">
+            <div class="_title">{{ brewery.name }}</div>
           </div>
-          <div v-if="brewery.name">
-            <div class="_title">Brewery:</div>
-            <div class="_info">{{ brewery.name }}</div>
+          <div v-if="brewery.brewery_type" class="_col">
+            <div class="_info-title">Type of Brewery:</div>
+            <div class="_info">{{ brewery.brewery_type | uppercase }}</div>
           </div>
-          <div v-if="brewery.brewery_type">
-            <div class="_title">Type of Brewery:</div>
-            <div class="_info">{{ brewery.brewery_type }}</div>
+          <div v-if="brewery.street" class="_col">
+            <div class="_info-title">Address:</div>
+            <div class="_info">{{ brewery.street | uppercase }}</div>
           </div>
-          <div v-if="brewery.street">
-            <div class="_title">Address:</div>
-            <div class="_info">{{ brewery.street }}</div>
+          <div v-if="brewery.phone" class="_col">
+            <div class="_info-title">Phone Number:</div>
+            <div class="_info">{{ brewery.phone | formatPhoneNumber }}</div>
           </div>
-          <div v-if="brewery.phone">
-            <div class="_title">Phone Number:</div>
-            <div class="_info">{{ brewery.phone }}</div>
-          </div>
-          <div v-if="brewery.website_url">
-            <div class="_title">Website:</div>
-            <a :href="brewery.website_url" class="_info" target="_blank" rel="noopener">{{
+          <div v-if="brewery.website_url" class="_col">
+            <div class="_info-title">Website:</div>
+            <a :href="brewery.website_url" class="_info _link" target="_blank" rel="noopener">{{
               brewery.website_url
             }}</a>
           </div>
@@ -106,34 +108,110 @@ export default {
       console.log(`this.currentPage → `, this.currentPage)
       this.types = res.data
       console.log(`clicked → `, 'clicked')
-      // if result > 10 and are no more breweries
     },
     updatePage(pageNum) {
       this.currentPage = pageNum
       this.submit()
     },
+    formatPhoneNumber(phoneNumberString) {
+      var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+      var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+      if (match) {
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+      }
+      return null
+    },
   },
+  filters: {
+    uppercase(e) {
+      return e.charAt(0).toUpperCase() + e.slice(1)
+    },
+    formatPhoneNumber(e) {
+      var cleaned = ('' + e).replace(/\D/g, '')
+      var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+      if (match) {
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+      }
+      return null
+    },
+  },
+  computed: {},
 }
 </script>
 
 <style lang="sass">
+body
+  background-color: #F2AA4CFF
 #app
   font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
   margin-top: 60px
-
-._brewery
+  color: #545a60
+._mke
+  width: 300px
+._main-title
+  font-size: 62px
+  margin: 0
+._filter-bar
   display: flex
+  justify-content: space-around
+  align-items: center
+  padding: 0 30vw
+  margin-top: 32px
+._filter
+  display: flex
+  &-title
+    font-size: 32px
+    font-weight: bold
+    margin-right: 12px
+._btn
+  background-color: #545a60
+  color: #f8f8ff
+  font-size: 16px
+  border-radius: 16px
+  border: none
+  padding: 12px
+  margin-left: 6px
+  &:hover
+    transform: translateY(-3px)
+  &:active
+    transform: translateY(-1px)
+._dropdown
+  border-radius: 8px
+  padding: 0 6px
+  border: none
+  background-color: #f8f8ff
+  margin-right: 12px
+._list
+  display: inline-flex
+  flex: 1
   flex-direction: column
+  flex-wrap: wrap
+._brewery
   list-style-type: none
-  border: solid 2px black
+  // border: solid 2px #545a60
   border-radius: 1em
   padding: 1em
   margin-bottom: 2em
+  background-color: #f8f8ff
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px
+  &:hover
+    transform: scale(1.1)
 ._title
+  font-size: 32px
   font-weight: bold
+  margin-bottom: 6px
+  color: #00539CFF
+._info-title
+  font-size: 16px
+  margin-bottom: 4px
 ._info
+  font-size: 24px
+  font-weight: 500
+._col
+  display: flex
+  flex-direction: column
+._link
+  color: #F2AA4CFF
+  text-decoration: underline
 </style>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,10 +1,54 @@
 <template>
   <div class="_pagination-wrapper">
-    <span v-if="showPreviousLink" class="_pagination-btn" @click="updatePage(currentPage - 1)"
-      ><</span
-    >
-    {{ currentPage }}
-    <span v-if="showNextLink" class="_pagination-btn" @click="updatePage(currentPage + 1)">></span>
+    <div v-if="showPreviousLink" class="_pagination-btn" @click="updatePage(currentPage - 1)">
+      <svg
+        width="25"
+        height="25"
+        viewBox="0 0 25 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M19.9814 12.6434H5.98145"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M12.9814 19.6434L5.98145 12.6434L12.9814 5.64337"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+    <span style="font-size: 32px; padding: 0 8px">{{ currentPage }}</span>
+    <div v-if="showNextLink" class="_pagination-btn" @click="updatePage(currentPage + 1)">
+      <svg
+        width="25"
+        height="25"
+        viewBox="0 0 25 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.60596 12.5667H19.606"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M12.606 5.56671L19.606 12.5667L12.606 19.5667"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -30,5 +74,8 @@ export default {
 
 <style lang="sass" scoped>
 ._pagination-btn
+  display: inline-flex
+  justify-content: center
+  align-items: center
   cursor: pointer
 </style>
